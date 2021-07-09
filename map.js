e388e9fc6a79ba75dba9ff217b0dcf214b605fb6
dazzel3 <aakk9350@naver.com>
2021-07-09 14:16:09 +0900

    marker 이미지경로 수정

--- map.js
+++ map.js
@@ -6,7 +6,7 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-var imageSrc = '/var/www/html/ecallassets/marker1.png.', // 마커이미지의 주소입니다    
+var imageSrc = './assets/marker1.png', // 마커이미지의 주소입니다    
     imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
